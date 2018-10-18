7e4a0c547677c4fef30b72d4dccd1f636339d2d9
Angel Matilla <angelmatilla1994@hotmail.com>
2018-10-18 17:10:38 +0200

    testing

---
+++ Angeliooo/configuration.c
@@ -0,0 +1,14 @@
+//Function for requesting the configuration to the user
+
+#include <ncurses.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+//#include <dos.h>
+
+void main(void){
+
+printf("hello RTES world!\n");
+//delay(5000);
+return 0;
+}
