7ee14de5c1e4fe200a350ca6a2031bd1852a5f2e
Marc <marcpuru@gmail.com>
2018-10-29 13:48:05 +0100

    Adding Block, Rectangle, Hat and Spaceship

--- tCells/include/figure_insert.h
+++ tCells/include/figure_insert.h
@@ -3,3 +3,7 @@
 void R_pentomino_insert(int, int, tCell *);
 void Acorn_insert(int, int, tCell *);
 void Diehard_insert(int, int, tCell *);
+void Block_insert(int, int, tCell *);
+void Rectangle_insert(int, int, tCell *);
+void Hat_insert(int, int, tCell *);
+void Spaceship_insert(int, int, tCell *);
